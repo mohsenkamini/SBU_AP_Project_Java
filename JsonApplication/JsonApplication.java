113cfc33e20ef33a8551d5aff481b7ad9288349c
mohsenkamini <mohsenkamini@gmail.com>
2023-06-30 16:41:44 +0330

    write json is working fine

---
+++ JsonApplication/JsonApplication.java
@@ -0,0 +1,52 @@
+
+import com.google.gson.*; 
+import java.io.*;
+import java.net.*;
+
+public class JsonApplication {
+ 
+ public static void main (String [] args) {
+
+   // write a java object to a json file:
+   Dummy dummy = new Dummy(1, "Mohsen", "Iranian");
+   writeJSON(dummy, "/tmp/somewhere.json");
+
+   
+ }
+ 
+ public static void writeJSON(Dummy obj, String fileAddress) { 
+   try {
+    Gson gson = new Gson();
+    FileWriter writer = new FileWriter(fileAddress);
+    gson.toJson(obj, writer);
+    writer.flush();
+    writer.close();
+   } catch (IOException io) {System.out.println("IO exception:" + io);};
+ }  
+ 
+ /*private static Student readJSON() throws FileNotFoundException { 
+    GsonBuilder builder = new GsonBuilder(); 
+    Gson gson = builder.create(); 
+    BufferedReader bufferedReader = new BufferedReader(
+       new FileReader("student.json"));   
+    
+    Student student = gson.fromJson(bufferedReader, Student.class); 
+    return student; 
+ } */
+}
+
+class Dummy {
+    private int id;
+    private String name;
+    private transient String nationality;
+
+    public Dummy(int id, String name, String nationality) {
+        this.id = id;
+        this.name = name;
+        this.nationality = nationality;
+    }
+    
+    public Dummy(int id, String name) {
+        this(id, name, null);
+    }
+}
