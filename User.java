63a34e3a6360fe28b2a35edaf718c11328d58f94
mohsenkamini <mohsenkamini@gmail.com>
2023-06-30 22:07:54 +0330

    fixed up User a bit

--- User.java
+++ User.java
@@ -1,14 +1,7 @@
-import sun.util.resources.LocaleData;
-
 import java.util.*;
 import java.time.*;
 
 class User {
-    String currentUser;
-
-    ArrayList<String> users = new ArrayList<String>();
-    ArrayList<String> passwords = new ArrayList<String>();
-    ArrayList<Transaction> transactions = new ArrayList<Transaction>();
 
     String username;
     String fullName;
@@ -17,62 +10,16 @@ class User {
     Double balance;
     String password;
     String email;
-    Boolean isLoggedIn;
-    Ticket[] bookedTickets;
+    ArrayList<Ticket> tickets= new ArrayList<Ticket>();
+    ArrayList<Transaction> transactions = new ArrayList<Transaction>();
 
-
-    public int findUser(String user) {
-        for (int i = 0; i < users.size(); i++)
-            if (users.get(i).equals(user))
-                return i;
-        return -1;
-    }
-
-    public void SignIn(String user, String pass1, String pass2) throws usernameAlreadyExistsException, passwordsDoNotMatchException {
-        for (int i = 0; i < users.size(); i++)
-            if (findUser(user) != -1)
-                throw new usernameAlreadyExistsException();
-        if (pass1.equals(pass2)) {
-            users.add(user);
-            passwords.add(pass1);
-        } else {
-            throw new passwordsDoNotMatchException();
-        }
-
-    }
-
-    public void LogIn(String user, String pass) throws wrongPasswordException, userDoesNotExistException {
-        boolean userExists = false;
-        for (int i = 0; i < users.size(); i++) {
-            if (users.get(i).equals(user)) {
-                userExists = true;
-                if (passwords.get(i).equals(pass)) {
-                    currentUser = users.get(i);
-                    isLoggedIn = true;
-                    break;
-                } else {
-                    throw new wrongPasswordException();
-                }
-            }
-        }
-
-        if (!userExists) {
-            throw new userDoesNotExistException();
-        }
-
-    }
-
-    public void changePassword(String oldPass, String newPass1, String newPass2) throws passwordsDoNotMatchException {
-        if (isLoggedIn) {
-            int index = findUser(currentUser);
-            if (oldPass.equals(passwords.get(index))) {
-                if (newPass1.equals(newPass2)) {
-                    passwords.set(index, newPass1);
-                } else
-                    throw new passwordsDoNotMatchException();
-            } else throw new passwordsDoNotMatchException(); //?
-
-        }
+    /*public void changePassword(String oldPass, String newPass1, String newPass2) throws passwordsDoNotMatchException {
+        if (oldPass.equals(passwords.get(index))) {
+            if (newPass1.equals(newPass2)) {
+                passwords.set(index, newPass1);
+            } else
+                throw new passwordsDoNotMatchException();
+        } else throw new passwordsDoNotMatchException(); //?
     }
 
     public void changeUsername(String newUser) {
@@ -81,6 +28,7 @@ class User {
             users.set(index, newUser);
         }
     }
+    */
 
     public void changePhoneNumber(String newNum) {
         setPhoneNumber(newNum);
@@ -99,7 +47,6 @@ class User {
     }
 
     public void addBalance(Double amount) {
-        if (isLoggedIn) {
             balance += amount;
             Transaction t = new Transaction();
             Date d = new Date();
@@ -115,14 +62,9 @@ class User {
             t.setPrice(amount);
             t.setType("Charge increase");
             transactions.add(t);
-
-        }
-
-
     }
 
     public void removeBalance(Double amount) {
-        if (isLoggedIn) {
             balance -= amount;
             Transaction t = new Transaction();
             Date d = new Date();
@@ -138,10 +80,6 @@ class User {
             t.setPrice(amount);
             t.setType("Charge reduction");
             transactions.add(t);
-
-        }
-
-
     }
 
     public void setUsername(String username) {
@@ -199,24 +137,20 @@ class User {
     public String getEmail() {
         return this.email;
     }
-
-    public void setIsLoggedIn(Boolean isLoggedIn) {
-        this.isLoggedIn = isLoggedIn;
+    // public void setTickets(Ticket[] bookedTickets) {
+        // this.tickets = bookedTickets;
+    // }
+    public void addTicket(Ticket ticket) {
+        this.tickets.add(ticket);
     }
 
-    public Boolean getIsLoggedIn() {
-        return this.isLoggedIn;
+    public Ticket[] getTickets() {
+        Ticket[] result = new Ticket[this.tickets.size()];
+        result = this.tickets.toArray(result);
+        return result;
     }
 
-    public void setBookedTickets(Ticket[] bookedTickets) {
-        this.bookedTickets = bookedTickets;
-    }
-
-    public Ticket[] getBookedTickets() {
-        return this.bookedTickets;
-    }
-
-    class wrongPasswordException extends Exception {
+    /*class wrongPasswordException extends Exception {
         wrongPasswordException() {
 
         }
@@ -246,6 +180,6 @@ class User {
 
         String msg = "Passwords do not match. Please try again.";
     }
-
+    */
 
 }
