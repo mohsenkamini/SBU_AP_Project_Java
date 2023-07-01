c9b64069518752e37c054800821a88933a5638b1
mohsenkamini <mohsenkamini@gmail.com>
2023-07-02 00:19:40 +0330

    api req and response is working like a charm in backendserver

--- APIResponse.java
+++ APIResponse.java
@@ -2,7 +2,12 @@
 import com.google.gson.*; 
 
 public class APIResponse {
+
     int statusCode;
     String message;
     JsonElement payload;
+    APIResponse(){
+        statusCode=500;
+        message="500";
+    }
 }
