e2e78b2ebf9a97c894de753f180150142ceb468c
mohsenkamini <mohsenkamini@gmail.com>
2023-07-02 00:47:52 +0330

    the username is not acquired in the payload and that was throwing exception. fixed.

--- BackendServer.java
+++ BackendServer.java
@@ -104,8 +104,9 @@ public class BackendServer {
                             break;
                         case "/user/tickets/":
                             result.statusCode = 200;
+                            System.out.println(req.payload.toString());
                             JsonObject payloadJsonUserTickets = req.payload.getAsJsonObject();
-                            ArrayList<Ticket> userTickets = db.userTickets(payloadJsonUserTickets.get("username").toString());
+                            ArrayList<Ticket> userTickets = db.userTickets(req.username);
                             String finalUserTickets = new Gson().toJson(userTickets, userTickets.getClass());
                             System.out.println("user tickets");
                             result.payload = new Gson().fromJson(finalUserTickets, JsonElement.class);
